e39a588151a9d27173982e9d007a5aae63b87b03
Ana <abarba1@trinity.edu>
2026-04-01 12:30:31 +0200

    Initial commit message

---
+++ server.js
@@ -0,0 +1,100 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+app.use(express.static('web'))
+
+// read pet.json
+function loadPet() {
+    const pet = JSON.parse(fs.readFileSync('pet.json', 'utf8'))
+
+    const now = Date.now()
+
+    if (!pet.lastHunger) pet.lastHunger = now
+    if (!pet.lastHappiness) pet.lastHappiness = now
+    if (!pet.lastEnergy) pet.lastEnergy = now
+    if (!pet.lastHealth) pet.lastHealth = now
+
+    return pet
+}
+
+// save pet.json back
+function savePet(pet) {
+    fs.writeFileSync('pet.json', JSON.stringify(pet, null, 2))
+}
+
+// decay
+function applyDecay(pet){
+    const now = Date.now()
+
+    const hungerHours = (now - pet.lastHunger) / 3600000
+    const happinessHours = (now - pet.lastHappiness) / 3600000
+    const energyHours = (now - pet.lastEnergy) / 3600000
+    const healthHours = (now - pet.lastHealth) / 3600000
+
+    pet.hunger = Math.max(0, Math.min(100, pet.hunger - (hungerHours * 2 )))
+    pet.happiness = Math.max(0, Math.min(100, pet.happiness - (happinessHours * 2 )))
+    pet.energy = Math.max(0, Math.min(100, pet.energy - (energyHours * 2 )))
+    pet.health = Math.max(0, Math.min(100, pet.health - (healthHours * 2 )))
+
+    return pet
+}
+
+
+// status endpoint for webpage
+app.get('/status', (req, res) => {
+    const pet = loadPet()
+    applyDecay(pet)
+    savePet(pet)
+    res.json(pet)
+})
+
+// HUNGER ----------
+app.post(['/feed', '/fish'], (req, res) => {
+    const pet = loadPet()
+    applyDecay(pet)
+
+    pet.hunger = Math.min(100, pet.hunger + 4)
+    pet.lastHunger = Date.now()
+
+    savePet(pet)
+    res.end()
+})
+
+// HAPPINESS ----------
+app.post(['/explore', '/pat'], (req, res) => {
+    const pet = loadPet()
+    applyDecay(pet)
+
+    pet.happiness = Math.min(100, pet.happiness + 2)
+    pet.lastHappiness = Date.now()
+
+    savePet(pet)
+    res.end()
+})
+
+//ENERGY ----------
+app.post(['/play', '/rest'], (req, res) => {
+    const pet = loadPet()
+    applyDecay(pet)
+
+    pet.energy = Math.min(100, pet.energy + 2)
+    pet.lastEnergy = Date.now()
+    
+    savePet(pet)
+    res.end()
+})
+
+// HEALTH ----------
+app.post(['/clean', '/train'], (req, res) => {
+    const pet = loadPet()
+    applyDecay(pet)
+
+    pet.health = Math.min(100, pet.health + 2)
+    pet.lastHealth = Date.now()
+
+    savePet(pet)
+    res.end()
+})
+
+app.listen(3000, () => console.log('running on port 3000'))
---
+++ web/app.js
@@ -0,0 +1,24 @@
+// fetch status from server every 3 seconds
+setInterval(() => {
+    fetch('https://virtualPet.mimobox.sh/status')
+        .then(res => res.json())
+        .then(pet => {
+
+            // update bars
+            document.getElementById('hunger-fill').style.width = pet.hunger + '%'
+            document.getElementById('happiness-fill').style.width = pet.happiness + '%'
+            document.getElementById('energy-fill').style.width = pet.energy + '%'
+            document.getElementById('health-fill').style.width = pet.health + '%'
+
+            // image swap
+            const img = document.getElementById('pixel-image')
+
+            if (pet.hunger >= 75 && pet.happiness >= 75 && pet.energy >= 75 && pet.health >=75){
+                img.src = 'happy.png'
+            } else if (pet.hunger <=45 || pet.happiness <= 45 || pet.energy <= 45 || pet.health <= 45 ){
+                img.src = 'sad.png'
+            } else {
+                img.src = 'neutral.png'
+            }
+        })
+}, 3000)
